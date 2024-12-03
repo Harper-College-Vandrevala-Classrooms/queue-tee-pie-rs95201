77524cdd79db195dbb413eeaa48f99f7c5c7afc7
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-12-02 21:19:23 -0600

    Update my_queue.hpp

--- my_queue.hpp
+++ my_queue.hpp
@@ -2,115 +2,109 @@
 #include <string>
 #include "cutie.hpp"
 
-#include "cutie.hpp"
-#include <string>
 class Puppy : public Cutie 
 {
-  std::string description() override 
-  {
-    return "A little puppy with big, sad eyes";
-  }
+public:
+    std::string description() override 
+    {
+        return "A little puppy with big, sad eyes";
+    }
 
-  int cuteness_rating() override 
-  {
-    return 11; 
-  }
+    int cuteness_rating() override 
+    {
+        return 11;
+    }
 };
-class Kitty : public Cutie
+
+class Kitty : public Cutie 
 {
-  std::string description() override 
-  {
-    return "A little Kitty with big, sad eyes";
-  }
+public:
+    std::string description() override 
+    {
+        return "A little Kitty with big, sad eyes";
+    }
 
-  int cuteness_rating() override 
-  {
-    return 8; 
-  }
+    int cuteness_rating() override
+    {
+        return 8;
+    }
 };
+
 class PygmyMarmoset : public Cutie
 {
-  std::string description() override 
-  {
-    return "A little PygmyMarmoset with big, sad eyes";
-  }
+public:
+    std::string description() override 
+    {
+        return "A little Pygmy Marmoset with big, sad eyes";
+    }
 
-  int cuteness_rating() override 
-  {
-    return 6; 
-  }
+    int cuteness_rating() override
+    {
+        return 6;
+    }
 };
 
 const int MAX_SIZE = 100;
 
-template <typename T>
-class MyQueue 
+class QueueTees 
 {
 private:
-    T* arr[MAX_SIZE]; 
-    T* top;           
-    T* start;          
+    Cutie* arr[MAX_SIZE];  
+    Cutie** top;  
+    Cutie** start;  
 
 public:
-    MyQueue()
-{
-        top = arr;   
-        start = arr; 
-}
+    QueueTees() 
+    {
+        top = arr;  
+        start = arr;  
+    }
 
-    bool is_empty() 
-{
+    bool is_empty()
+    {
         return (top == start); 
-}
+    }
 
-    bool is_full()
-{
+    bool is_full() 
+    {
         return (arr + MAX_SIZE == top); 
-}
+    }
 
-    void enqueue(T* element) 
-{  
+    void enqueue(Cutie* element) 
+    {
         if (!is_full()) 
         {
             *top = element;  
-            top++;           
-        }
-        else 
-        {
-          throw "Oh no!";
+            top++;  
         }
     }
 
-    T* dequeue()
-{  
-        if (!is_empty())
+    Cutie* dequeue() 
+    {
+        if (!is_empty()) 
         {
-            T* poppedElement = *start;  
+            Cutie* poppedElement = *start; 
             start++;  
-            return poppedElement; 
+            return poppedElement;  
         } 
-        else 
+        else
         {
-           throw "Oh no!";
+            throw "Oh no! Queue is empty!";
         }
     }
-
 
     int size() 
-{
+    {
         return top - start;  
     }
 
-    void pp()
-{  
-        T* temp = start;
+    void pp() 
+    {
+        Cutie** temp = start;  
         while (temp < top) 
-        {
-            std::cout << (*temp)->description() << " (Cuteness: " << (*temp)->cuteness_rating() << ")\n";
-            temp++;
+        { 
+            std::cout << (*temp)->description() << " with cuteness rating: " << (*temp)->cuteness_rating() << std::endl;
+            temp++;  
         }
-        std::cout << std::endl;
     }
 };
-
-
