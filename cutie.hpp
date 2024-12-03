cb919296b68299c9bb1154278152deba66f7f13e
rs95201 <114031245+rs95201@users.noreply.github.com>
2024-12-02 21:17:10 -0600

    Update cutie.hpp

--- cutie.hpp
+++ cutie.hpp
@@ -4,6 +4,6 @@ class Cutie
 {
     public:
     
-  virtual std::string description() = 0;     // All cuties need to have a description of what makes them cute.
-  virtual int cuteness_rating() = 0; // All cuties get a cuteness rating out of ten.
+  virtual std::string description() = 0;   
+  virtual int cuteness_rating() = 0; 
 };
